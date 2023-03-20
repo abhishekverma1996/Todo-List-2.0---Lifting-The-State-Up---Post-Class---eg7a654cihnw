8feb57abca60d8326d978800e78271e8ba026e36
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-03-20 10:39:26 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,28 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Inbox from "./Inbox";
 import Next7Days from "./Next7Days";
 import Today from "./Today";
 
 const list = [
-  { number: 1, title: "Let's complete this", date: new Date("9/27/2022") },
-  { number: 2, title: "Should sleep at 9pm", date: new Date("9/28/2022") },
-  { number: 3, title: "Should complete react", date: new Date("10/5/2022") },
+  { number: 1, title: "Let's complete this", date: (new Date()).toLocaleDateString("en-US") },
+  { number: 2, title: "Should sleep at 9pm", date: (new Date()).toLocaleDateString("en-US") },
+  { number: 3, title: "Should complete react", date: (new Date()).toLocaleDateString("en-US") },
 ];
+  
 
 const MainSection = (props) => {
-  const [filteredlist, setFilteredList] = useState(list);
-  const addToList = (obj) => {
-    list.push(obj);
-    setFilteredList(list);
-  };
+  const [mainlist, setli] = useState(list);
+  const [aya, setaya] = useState(0);
+  function appendList(nayaList){
+    
+    setli([...mainlist, nayaList]);
+  }
   
   return (
     <div className="main-section">
       {props.active === "INBOX" && (
-        <Inbox list={filteredlist} append={addToList} />
+        <Inbox list={mainlist} append={appendList}/>                                         
       )}
-      {props.active === "TODAY" && <Today list={filteredlist} />}
-      {props.active === "NEXT" && <Next7Days list={filteredlist} />}
+      {props.active === "TODAY" && <Today list={mainlist} />}
+      {props.active === "NEXT" && <Next7Days list={mainlist} />}
     </div>
   );
 };
